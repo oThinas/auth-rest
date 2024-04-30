a54f49da0db19464f08beace3e51da041ee584d7
oThinas <thiagomartinsprado@gmail.com>
2024-04-30 03:38:47 -0300

    feat: add authentication controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,32 @@
+import { createUser, getUserByEmail } from 'db/users';
+import express from 'express';
+import { authentication, random } from 'helpers';
+
+async function register(request: express.Request, response: express.Response) {
+  try {
+    const { email, password, username} = request.body;
+    if (!email || !password || !username) {
+      return response.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return response.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return response.status(200).json(user).end();
+  } catch(error) {
+    console.log(error);
+    return response.sendStatus(400);
+  }
+}
