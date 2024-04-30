94c1bd7a80299468b214cb8ccd26909999eb75c6
oThinas <thiagomartinsprado@gmail.com>
2024-04-30 06:24:11 -0300

    feat: add logged user on request

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import { get } from 'lodash';
 import { deleteUserById, getUserById, getUsers } from '../db/users';
 
-async function getAllUsers(_: any, response: express.Response) {
+async function getAllUsers(request: express.Request, response: express.Response) {
   try {
     const users = await getUsers();
 
-    return response.status(200).json(users);
+    const currentUserId = get(request, 'identity._id', '');
+    const formattedUsers = users.map((user) => {
+      const userModel = {
+        '_id': user._id,
+        'username': user.username,
+        'email': user.email,
+        '__v': user.__v,
+      };
+
+      if (user._id.toString() === currentUserId.toString()) {
+        return { ...userModel, isLogged: true }
+      }
+
+      return { ...userModel };
+    });
+
+    return response.status(200).json(formattedUsers);
   } catch (error) {
     console.log(error);
     return response.sendStatus(400);
